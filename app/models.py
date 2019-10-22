4e0e25a24cd96fd1ebc119800a68f46d3c064ff8
peninah-njeri <peninahgathuru@gmail.com>
2019-10-22 21:59:45 +0300

    add a class pitch and also define the relationship

--- app/models.py
+++ app/models.py
@@ -5,18 +5,27 @@ from . import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String())
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
 
-
+       
 class Role(db.Model):
-    __table__ = 'roles'
+    __tablename__ = 'roles'
 
     id = db.Column(db.Integer,primary_key =True)
-    name = db.Column(db.string(255))
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
 
     def __repr__(self):
         return f'User{self.name}'
+
+class Pitch(db.Model):
+    __tablename__ ='pitches'
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    category = db.Column(db.String(255), nullable=False)
