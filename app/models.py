830a93ff2085e4d1a94be24422beb87e0567289e
peninah-njeri <peninahgathuru@gmail.com>
2019-10-22 14:32:29 +0300

    Create a connection between roles and users

--- app/models.py
+++ app/models.py
@@ -6,9 +6,17 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User {self.username}'
 
 
 class Role(db.Model):
+    __table__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key =True)
+    name = db.Column(db.string(255))
+
+    def __repr__(self):
+        return f'User{self.name}'
