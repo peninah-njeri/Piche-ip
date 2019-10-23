7931c2b14a46091965b14829b853395cd6a6350d
peninah-njeri <peninahgathuru@gmail.com>
2019-10-23 09:57:48 +0300

    create a registration form to register new users

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,3 @@ from flask_login import login_required
 def login():
     return render_template('auth/login.html')
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-@login_required
-def new_comment(id):
